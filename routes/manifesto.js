8e144cbc4e76efbbfe794725483c88838308ed43
Ronnie Turner <ronnie.turner.303@gmail.com>
2021-10-10 17:27:53 -0600

    Create manifesto.js

---
+++ routes/manifesto.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const path = require("path");
+
+const router = express.Router();
+
+const rootDir = require("../util/paths");
+
+router.get("/manifesto", (req, res, next) => {
+  res.sendFile(path.join(rootDir, "views", "manifesto.html"));
+});
+
+module.exports = router;
